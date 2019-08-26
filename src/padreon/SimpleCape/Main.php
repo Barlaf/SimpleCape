4127e2d4a3b2631c79af8fcebf2db2063ecf5d4e
Barlaf <yarfardu09500@gmail.com>
2019-08-26 11:36:29 +0200

    Exeption cape doesn't exist
    Adding if(file_exists($path)) for check if the cape exist

--- src/padreon/SimpleCape/Main.php
+++ src/padreon/SimpleCape/Main.php
@@ -99,27 +99,32 @@ class Main extends PluginBase{
     public function createCape(Player $player, string $file, CommandSender $sender = null){
         $ex = '.png';
         $path = $this->getDataFolder() . $file . $ex;
-        $img = imagecreatefrompng($path);
-        $rgba = "";
-        for($y = 0; $y < imagesy($img); $y++) {
-            for($x = 0; $x < imagesx($img); $x++) {
-                $rgb = imagecolorat($img, $x, $y);
-                $r = ($rgb >> 16) & 0xFF;
-                $g = ($rgb >> 8) & 0xFF;
-                $b = $rgb & 0xFF;
-                $rgba .= chr($r).chr($g).chr($b).chr(255);
+        if(file_exists($path)) {
+            $img = imagecreatefrompng($path);
+            $rgba = "";
+            for($y = 0; $y < imagesy($img); $y++) {
+                for($x = 0; $x < imagesx($img); $x++) {
+                    $rgb = imagecolorat($img, $x, $y);
+                    $r = ($rgb >> 16) & 0xFF;
+                    $g = ($rgb >> 8) & 0xFF;
+                    $b = $rgb & 0xFF;
+                    $rgba .= chr($r).chr($g).chr($b).chr(255);
+                }
             }
-        }
-        if (!strlen($rgba) == 8192){
-            if (!$sender == null){
-                $sender->sendMessage(TextFormat::RED . "Invalid cape");
+            if (!strlen($rgba) == 8192){
+                if (!$sender == null){
+                    $sender->sendMessage(TextFormat::RED . "Invalid cape");
+                    return true;
+                }
+                $player->sendMessage(TextFormat::RED . "Invalid cape");
                 return true;
             }
-            $player->sendMessage(TextFormat::RED . "Invalid cape");
+            $this->setCape($player, $rgba, $sender);
+            return true;
+        } else {
+            $player->sendMessage(TextFormat::RED . "Cape doesn't exist");
             return true;
         }
-        $this->setCape($player, $rgba, $sender);
-        return true;
     }
 
     /**
